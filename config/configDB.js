ee84670f426fddecd2491ad5987a777f7ab7341f
Nguyễn Học <nguyenhoc@MacBook-Air-cua-Nguyen.local>
2022-05-10 21:14:55 +0700

    Add congig DB file

---
+++ config/configDB.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql')
+var config = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "cleanblog",
+    port: "3306"
+})
+module.exports = config;
